1a178c3bde87e82d64833f82745bb193f0df5014
Mhabibahmad <93278033+Mhabibahmad@users.noreply.github.com>
2022-01-26 22:50:15 +0500

    Create RLogubSchema.py

---
+++ RLogubSchema.py
@@ -0,0 +1,12 @@
+from lombok import AllArgsConstructor
+from lombok import Builder
+from lombok import Data
+from lombok import NoArgsConstructor
+from lombok import Value
+
+#ORIGINAL LINE: @Data @AllArgsConstructor @NoArgsConstructor @Builder(toBuilder = true) public class RLogubSchema
+class RLogubSchema:
+
+    def __init__(self):
+        #instance fields found by Java to Python Converter:
+        self.__schema = None
